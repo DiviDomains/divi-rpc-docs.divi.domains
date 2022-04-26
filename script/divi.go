2743a6561b8379240ba5c74e29eaaa9f9dfc780e
DiviDomains <89204767+DiviDomains@users.noreply.github.com>
2022-04-26 14:38:38 -0400

    Updated for Divi

--- script/divi.go
+++ script/divi.go
@@ -91,7 +91,7 @@ func open(path string) io.Writer {
 }
 
 func run(args ...string) string {
-	out, err := exec.Command("/home/marsh/v4.5.1-1_github_docs/zcash/src/zcash-cli", args...).CombinedOutput()
+	out, err := exec.Command("divi-cli", args...).CombinedOutput()
 	if err != nil {
 		panic(err)
 	}
